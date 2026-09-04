48b92b9bd8bfc918fa7812f03f9b5327bdbf74d1
lzhu19 <laura.zhu.2839@gmail.com>
2026-09-04 09:32:18 -0700

    Use GET endpoint to retrieve users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,6 +39,8 @@ const findUserByName = (name) => {
     );
 }
 
+const findUserById = (id) => users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => { // HTTP GET request
@@ -53,6 +55,16 @@ app.get("/users", (req, res) => {
         res.send(result);
     } else {
         res.send(users);
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; // or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
     }
 });
 
