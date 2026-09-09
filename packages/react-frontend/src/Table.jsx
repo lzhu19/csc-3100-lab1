14b6aeb48faee73c24840dc964390a898bc42e19
lzhu19 <laura.zhu.2839@gmail.com>
2026-09-08 23:28:21 -0700

    Show ID in table

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -6,6 +6,7 @@ function TableHeader() {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>ID</th>
       </tr>
     </thead>
   );
@@ -17,6 +18,7 @@ function TableBody(props) {
       <tr key={row.id}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>{row.id}</td>
         <td> 
           <button onClick = {() => props.removeCharacter(row.id)}>
             Delete
